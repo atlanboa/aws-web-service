8a0e3138ced590815f8435abe4bedb418b3551be
jaden <atlanboa@gmail.com>
2020-11-25 15:04:56 +0900

    travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ echo "> 현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
 	echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-	echo ">kill -15 $CURRENT_PID"
+	echo "> kill -15 $CURRENT_PID"
 	kill -15 $CURRENT_PID
 	sleep 5
 fi
@@ -23,8 +23,9 @@ echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/ | tail -n 1)
 
-echo ">JAR Name: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 실행권한 추가"
+cd $REPOSITORY
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
